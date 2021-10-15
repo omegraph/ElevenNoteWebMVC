64a9f7c0cfc5b8120137b0f0e5c4869ece250f9a
Samir Omerani <omegraph@gmail.com>
2021-10-15 18:58:02 -0400

    Final Commit

--- ElevenNote.Models/NoteDetail.cs
+++ ElevenNote.Models/NoteDetail.cs
@@ -1,31 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ElevenNote.Data
+namespace ElevenNote.Models
 {
-    public class Note
+    public class NoteDetail
     {
-        [Key]
         public int NoteId { get; set; }
-
-        [Required]
-        public Guid OwnerId { get; set; }
-
-        [Required]
         public string Title { get; set; }
-
-        [Required]
         public string Content { get; set; }
-
-        [Required]
+        [Display(Name = "Created")]
         public DateTimeOffset CreatedUtc { get; set; }
 
+        [Display(Name = "Modified")]
         public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
-
